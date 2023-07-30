8c14b72a2d34c4029605b095e40e55a38e01c7f0
salman-ahm3d <salmanahmed69890@gmail.com>
2023-07-30 01:47:48 -0600

    Update rps.js
    - Add getCPUDecision(), getPlayerDecision(), game() and main()
      functions to complete the rock paper scissor game

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,21 +1,59 @@
 function playRound(playerDecision, cpuDecision) {
-    gameDecision =  (playerDecision + cpuDecision).toLowerCase().trim();
+    let gameDecision =  (playerDecision + cpuDecision).toLowerCase().trim();
 
 
-    const playerWins = ["rockscissor", "scissorpaper", "paperrock"];
+    const playerWinConditions = ["rockscissor", "scissorpaper", "paperrock"];
 
     
 
     if (playerDecision == cpuDecision) {
-        console.log("Tie!");
         alert("Tie!");
-    } else if (playerWins.includes(gameDecision)) {
-        console.log("Player wins!");
+    } else if (playerWinConditions.includes(gameDecision)) {
         alert("Player wins!");
+        playerWins++;
     } else {
-        console.log("CPU Wins!");
         alert("CPU Wins!");
+        cpuWins++;
     }
 }
 
-playRound("paper","paper");
+function getCPUDecision() {
+    let choices = ["rock","paper","scissor"];
+    return choices[Math.floor(Math.random()*3)];
+}
+
+function getPlayerDecision() {
+    while (true) {
+        let playerDecision = prompt("Enter your choice: (rock/paper/scissor)").toLowerCase();
+        if (playerDecision.includes("rock")) {
+            return "rock";
+        } else if (playerDecision.includes("scissor")) {
+            return "scissor";
+        } else if (playerDecision.includes("paper")) {
+            return "paper";
+        } else {
+            alert("Invalid choice!");
+        }
+    }
+    
+}
+
+
+function game() {
+    let playerDecision = getPlayerDecision();
+    let cpuDecision = getCPUDecision();
+    playRound(playerDecision, cpuDecision);
+}
+
+function main() {
+    alert("Rock Paper Scissor");
+    while (cpuWins!=5 && playerWins!=5) {
+        game();
+    }
+}
+
+let cpuWins = 0;
+let playerWins = 0;
+
+
+main();
