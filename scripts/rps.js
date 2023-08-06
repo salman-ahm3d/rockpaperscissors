15becf19ebd6b46ae17f96b070a745564d4b21d8
salman-ahm3d <salmanahmed69890@gmail.com>
2023-08-06 06:24:08 -0600

    Update rps.js to add basic playability

--- scripts/rps.js
+++ scripts/rps.js
@@ -2,6 +2,10 @@ const buttons = document.querySelectorAll('.choice');
 let playerDecision;
 let cpuDecision;
 
+let playerWins = 0;
+let cpuWins = 0;
+let ties = 0;
+
 
 
 function getCPUDecision() {
@@ -13,12 +17,44 @@ function addEventsToButtons(buttons) {
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
             playerDecision = button.classList[1];
+            cpuDecision = getCPUDecision();
+            playRound(playerDecision,cpuDecision);
         })
     })
 }
 
-function game(playerDecision, cpuDecision) {
+function updateScore(gameResult) {
+    if (gameResult.includes('player')) {
+        console.log(gameResult);
+        playerWins++;
+        const result = document.querySelector('.player-wins');
+        result.textContent = `Player Wins: ${playerWins}`;
+    } else if (gameResult.includes('cpu')) {
+        cpuWins++;
+        const result = document.querySelector('.cpu-wins');
+        result.textContent = `CPU Wins: ${cpuWins}`;
+    } else {
+        ties++;
+        const result = document.querySelector('.ties');
+        result.textContent = `Ties: ${ties}`;
+    }
+}
+
+function playRound(playerDecision, cpuDecision) {
+    let gameDecision =  (playerDecision + cpuDecision).toLowerCase().trim();
+    console.log(playerDecision+cpuDecision);
+
+    const playerWinConditions = ["rockscissor", "scissorpaper", "paperrock"];
+
     
+
+    if (playerDecision == cpuDecision) {
+        updateScore('tie');
+    } else if (playerWinConditions.includes(gameDecision)) {
+        updateScore('player');
+    } else {
+        updateScore('cpu');
+    }
 }
 
 
