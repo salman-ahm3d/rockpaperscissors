414796c8cadd5d2aac49469f933b4b02f28cc780
salman-ahm3d <salmanahmed69890@gmail.com>
2023-08-06 05:57:18 -0600

    Update rps.js to refactor code for UI

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,59 +1,23 @@
-function playRound(playerDecision, cpuDecision) {
-    let gameDecision =  (playerDecision + cpuDecision).toLowerCase().trim();
+const buttons = document.querySelectorAll('.choice');
+let playerDecision;
+let cpuDecision = getCPUDecision();
 
 
-    const playerWinConditions = ["rockscissor", "scissorpaper", "paperrock"];
-
-    
-
-    if (playerDecision == cpuDecision) {
-        alert("Tie!");
-    } else if (playerWinConditions.includes(gameDecision)) {
-        alert("Player wins!");
-        playerWins++;
-    } else {
-        alert("CPU Wins!");
-        cpuWins++;
-    }
-}
 
 function getCPUDecision() {
-    let choices = ["rock","paper","scissor"];
-    return choices[Math.floor(Math.random()*3)];
+    const choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * 3)];
 }
 
-function getPlayerDecision() {
-    while (true) {
-        let playerDecision = prompt("Enter your choice: (rock/paper/scissor)").toLowerCase();
-        if (playerDecision.includes("rock")) {
-            return "rock";
-        } else if (playerDecision.includes("scissor")) {
-            return "scissor";
-        } else if (playerDecision.includes("paper")) {
-            return "paper";
-        } else {
-            alert("Invalid choice!");
-        }
-    }
-    
+function addEventsToButtons(buttons) {
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            playerDecision = button.classList[1];
+        })
+    })
 }
 
 
-function game() {
-    let playerDecision = getPlayerDecision();
-    let cpuDecision = getCPUDecision();
-    playRound(playerDecision, cpuDecision);
-}
+addEventsToButtons(buttons);
 
-function main() {
-    alert("Rock Paper Scissor");
-    while (cpuWins!=5 && playerWins!=5) {
-        game();
-    }
-}
-
-let cpuWins = 0;
-let playerWins = 0;
-
-
-main();
+console.log(cpuDecision+playerDecision);
