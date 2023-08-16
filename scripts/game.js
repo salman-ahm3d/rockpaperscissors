ff2baa5f77e828f1755dfafaf866c8f479002249
salman-ahm3d <salmanahmed69890@gmail.com>
2023-08-16 14:41:47 -0600

    Add functionality to reset button

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,34 @@
 let playerWins = 0;
 let cpuWins = 0;
 
+let playerDecision;
+let cpuDecision;
+
 
 const playerButtons = document.querySelector(".player-box .choice-container");
+const resetButton = document.querySelector(".reset-btn");
+const playerResults = document.querySelector(".player-wins");
+const cpuResults = document.querySelector(".cpu-wins");
+const result = document.querySelector(".result-box");
 
-console.log(playerButtons);
+
+function addEventListenerToResetButton() {
+    resetButton.addEventListener("click",resetGame);
+}
+
+function addEventListenerToPlayerButtons() {
+    playerButtons.forEach((button), () => {
+        playerDecision = button.classList[0];
+    })
+}
+
+function resetGame() {
+    playerWins = 0;
+    cpuWins = 0;
+    playerResults.textContent = `Player wins: ${playerWins}`;
+    cpuResults.textContent = `CPU wins: ${cpuWins}`;
+    result.textContent = `No one`;
+}
+
+addEventListenerToResetButton();
+    
