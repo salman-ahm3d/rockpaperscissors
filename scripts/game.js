aa9100d053bfe22b41ad197c02557060b3a91443
salman-ahm3d <salmanahmed69890@gmail.com>
2023-08-17 08:33:30 -0600

    Add basic playability without checking for 5 wins

--- scripts/game.js
+++ scripts/game.js
@@ -5,21 +5,31 @@ let playerDecision;
 let cpuDecision;
 
 
-const playerButtons = document.querySelector(".player-box .choice-container");
+const playerButtons = document.querySelectorAll(".player-box .choice-container button");
 const resetButton = document.querySelector(".reset-btn");
 const playerResults = document.querySelector(".player-wins");
 const cpuResults = document.querySelector(".cpu-wins");
 const result = document.querySelector(".result-box");
 
 
+
 function addEventListenerToResetButton() {
     resetButton.addEventListener("click",resetGame);
 }
 
-function addEventListenerToPlayerButtons() {
-    playerButtons.forEach((button), () => {
-        playerDecision = button.classList[0];
-    })
+
+function getCPUDecision() {
+    const choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * 3)];
+}
+
+function addHighlighting(button) {
+    button.classList.add('selected');
+    
+}
+
+function removeHighlighting(button) {
+    button.classList.remove('selected');
 }
 
 function resetGame() {
@@ -30,5 +40,46 @@ function resetGame() {
     result.textContent = `No one`;
 }
 
+
+function playRound(playerDecision, cpuDecision) {
+    let gameDecision =  (playerDecision + cpuDecision).toLowerCase().trim();
+    console.log(gameDecision);
+
+    const playerWinConditions = ["rockscissor", "scissorpaper", "paperrock"];
+
+    
+
+    if (playerDecision == cpuDecision) {
+        updateScore('Tie');
+    } else if (playerWinConditions.includes(gameDecision)) {
+        updateScore('Player');
+    } else {
+        updateScore('CPU');
+    }
+
+}
+
+function updateScore(decision) {
+    if (decision.includes('Player')) {
+        playerWins++;
+    } else if (decision.includes('CPU')) {
+        cpuWins++;
+    }
+    result.textContent = decision;
+    playerResults.textContent = `Player wins: ${playerWins}`;
+    cpuResults.textContent = `CPU wins: ${cpuWins}`;
+}
+
+function addEventListenerToPlayerButtons() {
+    playerButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            playerDecision = button.classList[0];
+            cpuDecision = getCPUDecision();
+            playRound(playerDecision, cpuDecision);
+        })
+    })
+}
+
 addEventListenerToResetButton();
+addEventListenerToPlayerButtons();
     
